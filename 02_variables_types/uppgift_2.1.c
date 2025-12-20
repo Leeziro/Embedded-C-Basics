a6d512c2421ff1030879717e61fa47c7840f0b15
Simon Birberg <simonbirberg@gmail.com>
2025-12-20 22:47:37 +0100

    Added excersise and Q/A

---
+++ 02_variables_types/uppgift_2.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int speed = 10;
+    float temp = 23.252543f;
+    char value = 'K';
+
+    printf("Speed is: %d\n", speed);
+    printf("temp is: %.4f\n", temp);
+    printf("%c is ready to be eaten \n", value);
+
+    return 0;
+}
